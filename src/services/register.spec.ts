69a8ce1abdbbf10bbfcd066c7c9c3a237dbc8c5a
Roger Oliveira <rogerpolvr@gmail.com>
2024-06-23 00:59:33 -0300

    test: add test for register use case using in memory repository

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,22 @@
-import { expect, test } from 'vitest';
+import { compare } from 'bcryptjs';
+import { describe, expect, it } from 'vitest';
 
-test("check if it works", () => {
-  expect(1 + 1).toBe(2);
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users.repository';
+import { RegisterService } from '@/services/register.service';
+
+describe("Register Use Case", () => {
+  it("should hash user password upon registration", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterService(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    const isPasswordHashValid = await compare("123456", user.password_hash);
+
+    expect(isPasswordHashValid).toBe(true);
+  });
 });
