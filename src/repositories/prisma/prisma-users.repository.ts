20d4c9a18e46d3f669b07c86f3dbce171eae0d25
Roger Oliveira <rogerpolvr@gmail.com>
2024-06-23 00:58:10 -0300

    feat: create in-memory-users repository

--- src/repositories/prisma/prisma-users.repository.ts
+++ src/repositories/prisma/prisma-users.repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma';
-import { Prisma, PrismaClient, User } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 
 import { UsersRepository } from '../users.repository';
 
