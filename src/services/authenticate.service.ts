5223e21c783dafc4950d88aa370b6d07ead1e4b8
Roger Oliveira <rogerpolvr@gmail.com>
2024-06-26 00:02:04 -0300

    feat: add authenticate service

---
+++ src/services/authenticate.service.ts
@@ -0,0 +1,40 @@
+import { compare } from "bcryptjs";
+
+import { UsersRepository } from "@/repositories/users.repository";
+import { User } from "@prisma/client";
+
+import { InvalidCredentialsError } from "./errors/invalid-credentials.error";
+
+interface AuthenticateServiceRequest {
+  email: string;
+  password: string;
+}
+
+interface AuthenticateServiceResponse {
+  user: User;
+}
+
+export class AuthenticateService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute(
+    email: string,
+    password: string
+  ): Promise<AuthenticateServiceResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatch = await compare(password, user.password_hash);
+
+    if (!doesPasswordMatch) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      user,
+    };
+  }
+}
