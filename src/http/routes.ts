389cb6c2be1f3656ef5029af02bf68ca0d49201b
Roger Oliveira <rogerpolvr@gmail.com>
2024-06-27 00:17:46 -0300

    feat: create sessions route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -4,4 +4,5 @@ import { register } from './controllers/register.controller';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
+  app.post("/sessions", register);
 }
