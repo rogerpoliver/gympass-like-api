c7b0363e53b85ba8c897237a71617d981686603e
Roger Oliveira <rogerpolvr@gmail.com>
2024-06-27 00:19:42 -0300

    feat: create authenticate controller

---
+++ src/http/controllers/authenticate.controller.ts
@@ -0,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users.repository';
+import { AuthenticateService } from '@/services/authenticate.service';
+import { InvalidCredentialsError } from '@/services/errors/invalid-credentials.error';
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const authenticateService = new AuthenticateService(prismaUsersRepository);
+
+    await authenticateService.execute({ email, password });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message });
+    }
+
+    return reply.status(500).send();
+  }
+  return reply.status(200).send();
+}
