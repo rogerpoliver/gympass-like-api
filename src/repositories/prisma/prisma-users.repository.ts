e0060cfeaf51020e2bad7b431c4a130cd64b06b3
Roger Oliveira <rogerpolvr@gmail.com>
2024-06-29 01:09:48 -0300

    feat: create get user profile use case

--- src/repositories/prisma/prisma-users.repository.ts
+++ src/repositories/prisma/prisma-users.repository.ts
@@ -4,6 +4,9 @@ import { Prisma, User } from '@prisma/client';
 import { UsersRepository } from '../users.repository';
 
 export class PrismaUsersRepository implements UsersRepository {
+  findById(id: string): Promise<User | null> {
+    throw new Error("Method not implemented.");
+  }
   async create(data: Prisma.UserCreateInput) {
     return await prisma.user.create({ data });
   }
