8e5646767e32e14539374b0e6c024e338018a671
Roger Oliveira <rogerpolvr@gmail.com>
2024-06-30 21:40:00 -0300

    refactor: change fix id for randomUUID on create user

--- src/repositories/in-memory/in-memory-users.repository.ts
+++ src/repositories/in-memory/in-memory-users.repository.ts
@@ -1,3 +1,5 @@
+import { randomUUID } from 'node:crypto';
+
 import { Prisma, User } from '@prisma/client';
 
 import { UsersRepository } from '../users.repository';
@@ -7,7 +9,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: "user-1",
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
