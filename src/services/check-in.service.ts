606d33a07ff365d3d9279571f7e2537fdef186e6
Roger Oliveira <rogerpolvr@gmail.com>
2024-07-08 01:05:45 -0300

    feat: add a rule to not check-in on a distant gym

--- src/services/check-in.service.ts
+++ src/services/check-in.service.ts
@@ -1,8 +1,9 @@
-import { CheckInsRepository } from '@/repositories/check-ins.repository';
-import { GymsRepository } from '@/repositories/gyms.repository';
-import { CheckIn } from '@prisma/client';
+import { CheckInsRepository } from "@/repositories/check-ins.repository";
+import { GymsRepository } from "@/repositories/gyms.repository";
+import { CheckIn } from "@prisma/client";
 
-import { ResourceNotFoundError } from './errors/resource-not-found.error';
+import { ResourceNotFoundError } from "./errors/resource-not-found.error";
+import { getDistanceBetweenCoordinates } from "./utils/get-distance-between-coordinates";
 
 interface CheckInServiceRequest {
   userId: string;
@@ -24,6 +25,8 @@ export class CheckInService {
   async execute({
     userId,
     gymId,
+    userLatitude,
+    userLongitude,
   }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
     const gym = await this.gymsRepository.findById(gymId);
 
@@ -31,7 +34,22 @@ export class CheckInService {
       throw new ResourceNotFoundError();
     }
 
-    // TODO:  calcule a distancia
+    const distance = getDistanceBetweenCoordinates(
+      {
+        latitude: userLatitude,
+        longitude: userLongitude,
+      },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      }
+    );
+
+    const MAX_DISTANCE_IN_KILOMETERS = 0.1;
+
+    if (distance > MAX_DISTANCE_IN_KILOMETERS) {
+      throw new Error();
+    }
 
     const checkInOnSameDate = await this.checkInsRepository.findByUserIdOrDate(
       userId,
