44718794944f053663a801bf3146003da8727f61
Roger Oliveira <rogerpolvr@gmail.com>
2024-08-03 01:40:16 -0300

    chore: create a test env for controllers

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,15 @@
+import type { Environment } from "vitest";
+
+export default (<Environment>{
+	name: "prisma",
+	transformMode: "ssr",
+	async setup() {
+		console.log("Setup");
+
+		return {
+			async teardown() {
+				console.log("Teardown");
+			},
+		};
+	},
+});
