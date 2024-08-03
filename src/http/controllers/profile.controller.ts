cc09f0276e30e88f4cb24f466c68e0d75863770f
Roger Oliveira <rogerpolvr@gmail.com>
2024-08-03 00:50:57 -0300

    feat: implements a authentication middleware for app routes

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,13 +1,16 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import { makeGetUserProfileUseCase } from "@/services/factories/make-get-user-profile-use-case";
+
+import type { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-	await request.jwtVerify();
-
 	const getUserProfile = makeGetUserProfileUseCase();
-
-	const profile = await getUserProfile.execute({ userId: request.user.sub });
-	return reply.status(200).send();
-}
-function makeGetUserProfileUseCase() {
-	throw new Error("Function not implemented.");
+	const { user } = await getUserProfile.execute({
+		userId: request.user.sub,
+	});
+	return reply.status(200).send({
+		user: {
+			...user,
+			password_hash: undefined,
+		},
+	});
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,11 +1,14 @@
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 
 import { authenticate } from "./controllers/authenticate.controller";
 import { profile } from "./controllers/profile.controller";
 import { register } from "./controllers/register.controller";
+import { verifiyJwt } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post("/users", register);
 	app.post("/sessions", authenticate);
-	app.get("/me", profile);
+
+	// Needs authentication
+	app.get("/me", { onRequest: [verifiyJwt] }, profile);
 }
