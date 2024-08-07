c9335ea7934a07a3c3d79c3ea3d8002dabfc3b4a
Roger Oliveira <rogerpolvr@gmail.com>
2024-08-06 23:40:09 -0300

    fix: update type import for fastify server

--- src/http/controllers/authenticate.controller.ts
+++ src/http/controllers/authenticate.controller.ts
@@ -1,4 +1,4 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 import { InvalidCredentialsError } from "@/services/errors/invalid-credentials.error";
--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,4 +1,4 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 import { UserAlreadyExistsError } from "@/services/errors/user-already-exists.error";
