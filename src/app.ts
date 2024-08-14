ba4c201c9674f7c6ac62f5bf95534cd46375df91
Roger Oliveira <rogerpolvr@gmail.com>
2024-08-14 00:00:07 -0300

    feat: add refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { ZodError } from "zod";
 
+import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
 
 import { env } from "./env";
@@ -12,7 +13,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
 	secret: env.JWT_SECRET,
+	cookie: {
+		cookieName: "refreshToken",
+		signed: false,
+	},
+	sign: {
+		expiresIn: "10m",
+	},
 });
+
+app.register(fastifyCookie);
 
 app.register(userRoutes);
 app.register(gymsRoutes);
