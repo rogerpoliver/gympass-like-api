be3ef153a758fc44576b3c1ec7d9fd91c49f8933
Roger Oliveira <rogerpolvr@gmail.com>
2024-08-14 00:21:57 -0300

    feat: add refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,11 +4,14 @@ import { verifiyJwt } from "@/http/middlewares/verify-jwt";
 
 import { authenticate } from "./authenticate.controller";
 import { profile } from "./profile.controller";
+import { refresh } from "./refresh.controller";
 import { register } from "./register.controller";
 
 export async function userRoutes(app: FastifyInstance) {
 	app.post("/users", register);
 	app.post("/sessions", authenticate);
+
+	app.patch("/token/refresh", refresh);
 
 	// Needs authentication
 	app.get("/me", { onRequest: [verifiyJwt] }, profile);
